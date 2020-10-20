b0d97a831620bdb3be3d00307e8f02f9c2428170
dev-mbusch <dev.mbusch@gmail.com>
2020-10-20 14:05:18 +0200

    Day06 added to solution worked for 10x10

--- day06_fire.py
+++ day06_fire.py
@@ -7,14 +7,13 @@ https://adventofcode.com/2015/day/6
 
 import re
 from pprint import pprint as pp
-from re import findall
 
 # Grid Initilization
 gridsize = 10
-grid = [[0 for _i in range(gridsize)] for _j in range(gridsize)]
+init_grid = [[0 for _i in range(gridsize)] for _j in range(gridsize)]
 
 
-def find_action(instructions: str) -> str:
+def parse_action(instructions: str) -> str:
     action = ""
 
     if instructions.find("turn on") > -1:
@@ -25,12 +24,8 @@ def find_action(instructions: str) -> str:
         action = "toggle"
 
     return action
-# turn on 458,710 through 688,847
-# toggle 12,484 through 878,562
-# turn off 241,964 through 799,983
-#print(find_action("turn off 241,964 through 799,983"))
 
-def find_coordinates(instructions: str):
+def parse_coordinates(instructions: str):
 
     coordinates = re.findall("\d+",instructions)
     start = (int(coordinates[0]), int(coordinates[1]))
@@ -38,22 +33,83 @@ def find_coordinates(instructions: str):
 
     return start, end
 
-def calculate_switch_plan(start_grid, start_point, end_point):
+def set_switch_plan(start_grid, start_point, end_point, action):
     end_grid = start_grid
     x1, y1 = start_point
     x2, y2 = end_point
+    offset_x = x1
+    offset_y = y1
 
     for i, row in enumerate(end_grid[x1:x2+1]):
+
         for j, column in enumerate(row[y1:y2+1]):
-            end_grid[i][j] = 1
+            if action == 'toggle':
+                end_grid[j + offset_x][i + offset_y] = int(not end_grid[j][i])
+            elif action == 'turn_on':
+                end_grid[j + offset_x][i + offset_y] = 1
+            elif action == 'turn_off':
+                end_grid[j + offset_x][i + offset_y] = 0
+            else:
+                raise ValueError(f'The provided action ´{action}´ is not a valid operation.')
 
     return end_grid
 
+def compute_lights_on(grid):
+    counter_lights_on = 0
 
-start, end = find_coordinates("turn on 0,0 through 2,2")
+    for row in grid:
+        counter_lights_on += row.count(1)
 
-pp(grid)
+    return counter_lights_on
 
-calculate_switch_plan(grid, start, end)
-print('\n####################\n')
-pp(grid)
+# Tests Part One 10x10 Grid
+test_grid = []
+
+assert compute_lights_on(set_switch_plan(init_grid, (0,0), (9,9), 'turn_on')) == 100 # Works fine
+test_grid = set_switch_plan(init_grid, (0,0), (9,9), 'turn_on')
+
+print('\n', '#' * 40, '\n')
+pp(test_grid)
+print('\n', '#' * 40, '\n')
+
+assert compute_lights_on(set_switch_plan(test_grid, (0,0), (9,0), 'toggle')) == 90 # Works fine
+test_grid = set_switch_plan(test_grid, (0,0), (9,0), 'toggle')
+print(compute_lights_on(test_grid))
+
+print('\n', '#' * 40, '\n')
+pp(test_grid)
+print('\n', '#' * 40, '\n')
+
+# assert compute_lights_on(set_switch_plan(test_grid, (4,4), (5,5), 'turn_off')) == 86 # Works fine
+# test_grid = set_switch_plan(test_grid, (4,4), (5,5), 'turn_off')
+
+# print('\n', '#' * 40, '\n')
+# pp(test_grid)
+# print('\n', '#' * 40, '\n')
+
+# assert compute_lights_on(set_switch_plan(test_grid, (0,0), (999,0), 'turn_on')) == 1_000
+
+# WRONG ANSWER: 535162 --> "TOO LOW"
+
+# if __name__ == '__main__':
+#     with open('day06_input_mb.txt', 'r') as fh:
+#     # with open('day06_input_test_smallgrid.txt', 'r') as fh:
+#         grid = []
+#         # print('Empty grid:')
+#         # print('\n', '#' * 40, '\n')
+#         # pp(grid)
+#         # print('\n', '#' * 40, '\n')
+#         # print('Initialized Grid:')
+#         # print('\n', '#' * 40, '\n')
+#         # pp(init_grid)
+#         # print('\n', '#' * 40, '\n')
+
+#         for line in fh.readlines():
+#             start, end = parse_coordinates(line)
+#             action = parse_action(line)
+#             # print(f'Start Point: {start}, \t End Point: {end}, \t Action: {action}')
+#             grid = set_switch_plan(init_grid, start, end, action)
+#             # print('\n', '#' * 40, '\n')
+#             # pp(grid)
+#             # print('\n', '#' * 40, '\n')
+#         print(f'Number of lights switched on: {compute_lights_on(grid)}.')
